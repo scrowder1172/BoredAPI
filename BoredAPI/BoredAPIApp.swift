dc592edef667c1826b73f2cde5f3901f587a67a2
Scott Crowder <scrowder@gmail.com>
2024-05-16 12:01:39 -0500

    Initial Commit

---
+++ BoredAPI/BoredAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredAPIApp.swift
+//  BoredAPI
+//
+//  Created by SCOTT CROWDER on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BoredAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
